f0c152a11a7f78795fa0cc9837413fce1f4d8277
gust-soares <>
2025-11-05 13:21:05 -0300

    feat: configuring the API

--- notable_django/notable_django/urls.py
+++ notable_django/notable_django/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+
+from django.urls import path, include, re_path
+from django.contrib import admin
+from api.resources import NoteResource
+note_resource = NoteResource()
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api/', include(note_resource.urls)),
 ]
+
